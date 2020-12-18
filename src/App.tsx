1818955dbfddff6c585e696dd35532eda1714bac
Petar Dzhambazov <petar.dzhambazov@ezbob.com>
2020-12-18 17:36:46 +0200

    remove base styling, update schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import ConfigEditor from "./components/ConfigEditor";
-import logo from "./logo.svg";
-import "./App.css";
 
 import config from "./config/config";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit config and save.</p>
-      </header>
       <div className="content">
         <ConfigEditor
           config={config}
